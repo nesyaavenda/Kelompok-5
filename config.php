0bd7513133d3ff756eb470a12651c69cd5937d32
nesyaavenda <82674268+nesyaavenda@users.noreply.github.com>
2021-04-19 16:28:07 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+
+
+date_default_timezone_set("Asia/Jakarta");
+error_reporting(0);
+
+	
+	$host 	= 'localhost'; 
+	$user 	= 'root';  
+	$pass 	= ''; 
+	$dbname = 'db_toko'; 
+	
+	try{
+		$config = new PDO("mysql:host=$host;dbname=$dbname;", $user,$pass);
+	
+	}catch(PDOException $e){
+		echo 'KONEKSI GAGAL' .$e -> getMessage();
+	}
+	
+	$view = 'fungsi/view/view.php';
+?>
+
